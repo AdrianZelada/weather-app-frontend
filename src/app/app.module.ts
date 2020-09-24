a8cbf3c26a40443c176d8032e9ac121c621b5d95
Adrian Zelada Torrez <zelada_torrez@hotmail.com>
2020-09-24 01:02:35 -0400

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { CardWeatherComponent} from './card-weather/card-weather.component';
+import { PanelWeatherComponent} from './panel-weather/panel-weather.component';
+import { TitleCityComponent} from './title-city/title-city.component';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatGridListModule} from '@angular/material/grid-list';
 import { MatListModule} from '@angular/material/list';
@@ -17,7 +20,10 @@ import { MatInputModule} from '@angular/material/input'
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarCountriesComponent
+    SidebarCountriesComponent,
+    CardWeatherComponent,
+    PanelWeatherComponent,
+    TitleCityComponent
   ],
   imports: [
     BrowserModule,
